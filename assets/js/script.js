41cb792052abe4b0e43ae2b1308c145a5cfeca8f
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-04 14:36:41 +0200

    ex1 partie1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 //onmouseover  survol
 //onmouseout survol et vision d'une autre image
+var image= document.getElementById('photo');
+
+function functionPhoto(){
+  image.src='assets/image/trop bien.png';
+}
+function functionPhotoBack(){
+  image.src='assets/image/occupée.png';
+}
